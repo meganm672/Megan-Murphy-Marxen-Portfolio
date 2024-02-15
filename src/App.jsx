e28758c5403dee932bb0790f956b3d066b3e7385
meganm672 <meganm672@gmail.com>
2024-02-15 11:54:25 -0500

    updates with nav and react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,26 @@ import About from './components/About';
 import './App.css'
 import Footer from './components/Footer';
 import Projects from './components/Projects';
+import Home from './components/Home'
+import Navbar from './components/Navbar';
+import Resume from './components/Resume';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  
+
 
   return (
     <>
-      <About />
-      <Projects />
-      <ToolKit/>
-      <Connect />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/project' element={<Projects />} />
+        <Route path='/toolkit' element={<ToolKit />} />
+        <Route path='/connect' element={<Connect />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/footer' element={<Footer />} />
+      </Routes>
       <Footer />
     </>
   )
