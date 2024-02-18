f67c60e9d4ac14c8325124f59cbb0ea011feed61
meganm672 <meganm672@gmail.com>
2024-02-18 16:05:34 -0500

    portfolio updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import MeganMurphyMarxen from "../assets/MeganMurphyMarxen.jpg"
 import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography';
+
 const About = () => {
   return (
     <>
